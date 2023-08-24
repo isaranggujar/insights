76793caaab642008cd794a591c7f91209d500bdf
isaranggujar <isaranggujar@gmail.com>
2023-08-24 23:56:28 +0530

    Change in list

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-thislist = ["apple", "mango", "orange", "banana", "cherry"]
-print("Gess a Word.")
-a = input()
-if a in thislist:
-    print("Wow, Yes it's Available!")
-else:
-    print("Try Again!")
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
