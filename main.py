049cbdffc676a3d7fb1951ff1b3b3c4c2ec2cfdb
isaranggujar <isaranggujar@gmail.com>
2023-09-07 18:46:16 +0530

    Update on 07-09-2023

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-thisdict ={"brand": "color"}
-print(thisdict)
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name} {self.age}"
+
+p1 = Person("John", 36)
+
+print(p1)
