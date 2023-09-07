ccb06e2159edabcc889b931975d2eefff51f6cb2
isaranggujar <isaranggujar@gmail.com>
2023-09-07 20:20:16 +0530

    Update on 07-09-2023

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+import datetime
 
-  def __str__(self):
-    return f"{self.name} {self.age}"
-
-p1 = Person("John", 36)
-
-print(p1)
+x = datetime.datetime.now()
+print(x)
