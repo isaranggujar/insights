366bc1a5cb2e2c79f711fe449ecdf4e2ba215886
isaranggujar <isaranggujar@gmail.com>
2023-10-02 20:58:55 +0530

    Update on 29-09-2023

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-"""mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")"""
-print(mydb)
+'mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")'
+mycursor.execute("DROP TABLE customers")
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
