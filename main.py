5ef4934c2978cdde3877e4a80862d150a7f48b24
saran <isaranggujar@gmail.com>
2023-10-11 17:15:58 +0530

    Updated on 11/10/2023

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-import mysql.connector
+import matplotlib.pyplot as plt
+import numpy as np
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="Admins@2023",
-  database="mydb"
-)
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
 
-mycursor = mydb.cursor()
-
-'mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")'
-mycursor.execute("DROP TABLE customers")
-mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("John", "Highway 21")
-mycursor.execute(sql, val)
-
-mydb.commit()
-
-print(mycursor.rowcount, "record inserted.")
+plt.plot(xpoints, ypoints)
+plt.show()
